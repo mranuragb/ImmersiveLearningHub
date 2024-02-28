41dc25510a22cd5968f265c77e3b88c20bb76b25
mranuragb <bawaneanurag@gmail.com>
2024-02-29 00:49:10 +0530

    Add a new Schema

---
+++ BackEndPro/models/OTP.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const mailsender = require("../utils/mailSender");
+
+const OTPSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+    },
+    otp:{
+        type:String,
+        required:true,
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now(),
+        expires:5*60,
+    }
+});
+
+//A Function -> to send Emails
+
+async function sendVerificationEmail(email,opt){
+    try{
+        const mailResponse = await mailsender(email,"verification Email from StudyNotaion",otp);
+        console.log("Email sent Successfully : ",mailResponse);
+    }
+    catch(error){
+        console.log("Error occured while sending mail",error);
+        throw error;
+    }
+}
+
+OTPSchema.pre("save",async function(next){
+    await sendVerificationEmail(this.email,this.otp);
+    next();
+})
+
+module.exports = mongoose.model("OTP",OTPSchema);
